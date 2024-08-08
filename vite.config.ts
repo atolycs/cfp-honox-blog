9bb784e924b242a2e4ec45cd164d0ab3f9568d34
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-08 16:40:31 +0900

    init: setup environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,50 @@
 import pages from '@hono/vite-cloudflare-pages'
 import adapter from '@hono/vite-dev-server/cloudflare'
 import honox from 'honox/vite'
+import client from "honox/vite/client"
+import ssg from "@hono/vite-ssg"
+import mdx from "@mdx-js/rollup"
+import rehypeHighlight from "rehype-highlight"
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), pages()]
+
+
+const entry = "./app/server.ts"
+
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+      build: {
+        rollupOptions: {
+          input: ["/app/style.css"],
+          output: {
+            assetFileNames: "static/assets/[name].[ext]"
+          }
+        }
+      }
+    }
+  }
+
+  return {
+    build: {
+      emptyOutDir: false
+    },
+
+    plugins: [
+      honox(),
+      pages(),
+      mdx({
+        jsxImportSource: "hono/jsx",
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+        rehypePlugins: [rehypeHighlight]
+      }),
+      ssg({ entry })
+    ]
+  }
 })
+
+
