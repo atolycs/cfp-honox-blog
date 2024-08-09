3de1a21a5e463a90ce8570150108dea2da2dd729
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-09 09:04:42 +0900

    init: blog template add

---
+++ app/config/site_config.ts
@@ -0,0 +1,17 @@
+
+import { SITEDefine, SocialsDefine } from '../types'
+
+export const SITE: SITEDefine = {
+    title: 'Tech Memo',
+    description: 'しがないエンジニアのテックメモなどのチラ裏',
+    author: 'Atolycs'
+}
+
+export const SOCIALS: SocialsDefine = [
+    {
+        name: "Github",
+        href: "https://github.com/atolycs",
+        active: true,
+        linkdescription: "Atolycs on Github"
+    }
+] 
---
+++ app/types.ts
@@ -0,0 +1,12 @@
+export type SITEDefine = {
+    title: string
+    description: string
+    author: string
+}
+
+export type SocialsDefine = {
+    name: string
+    href: string
+    active: boolean
+    linkdescription: string
+}[]
