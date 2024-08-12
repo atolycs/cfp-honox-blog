1e50e822390bc24fc478b5849a6b5e380d48329e
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-13 08:59:38 +0900

    feat: add function

--- app/lib/getPost.ts
+++ app/lib/getPost.ts
@@ -1,28 +1,27 @@
-
-import type { MDX } from "../types/MDX"
-import { baseName } from "./path-utils"
+import type { MDX } from "../types/MDX";
+import { baseName } from "./path-utils";
 
 const posts = import.meta.glob<MDX>("../content/blog/*.md", {
-    eager: true
-})
+  eager: true,
+});
 
-export const getPosts = ()=> {
-    const postData = Object.entries(posts)
-    .map(([path, post]) => {
-        const entryName = baseName(path)
-        const { frontmatter } = post
-        const { default: Component } = post
-        return {
-            entryName,
-            frontmatter,
-            Component
-        }
-    })
+export const getPosts = () => {
+  const postData = Object.entries(posts).map(([path, post]) => {
+    const entryName = baseName(path);
+    const { frontmatter } = post;
+    const { default: Component } = post;
+    return {
+      entryName,
+      frontmatter,
+      Component,
+    };
+  });
 
-    return postData
-}
+  return postData;
+};
 
 export const getPostEntryName = (entryName: string) => {
-    const posts = getPosts();
-    const post = posts.find((post) => post.entryName === entryName)
-}
+  const posts = getPosts();
+  const post = posts.find((post) => post.entryName === entryName);
+  return post;
+};
