31f38e406243333241c616c004a5eed89b073ecc
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-13 12:25:42 +0900

    feat: techinfo added

---
+++ app/islands/techinfo.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "hono/jsx";
+import { Link } from "./Link";
+
+
+export const TechInfo: FC = () => {
+    return (
+      <span>
+        Powered by <Link href="https://hono.dev" className="hover:underline">Hono</Link> and 
+        <Link href="https://pages.cloudflare.com/" className="hover:underline">Cloudflare Pages</Link>
+      </span>
+    );
+}
