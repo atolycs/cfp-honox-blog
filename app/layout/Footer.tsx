9760e95638ab30563aaa816929a783b0bae445f6
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-13 12:08:25 +0900

    chore: fix design

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -1,21 +1,11 @@
 import type { FC } from "hono/jsx";
 import { css } from "hono/css";
 
-const footerStyle = css`
-    background-color: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px;
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-`;
 
 export const Footer: FC = () => {
   const year = new Date().getFullYear();
   return (
-    <footer class={footerStyle}>
+    <footer className="flex justify-center items-center my-6">
       <p> Copyright {year} Atolycs</p>
     </footer>
   );
--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -10,19 +10,17 @@ type Props = {
 
 export const Header = (props: Props) => {
   return (
-    <header className="sticky top-0 z-10 border-zinc-200 border-b py-3 backdrop-blue">
-      <div className="mx-auto flex max-w-3xl items-center justify-between px-4">
-        <Link href="/" className="text-lg font-semibold">
-          {SITE.title}
+    <header className="text-center border-b px-4 mx-2 max-md:px-2 w-full h-16 tracking-widest flex justify-between items-center">
+      <Link href="/" className="text-lg font-semibold">
+        {SITE.title}
+      </Link>
+      <div className="flex justify-items-center gap-4">
+        <Link href="/about" className="text-lg font-semibold">
+          About
         </Link>
-        <div className="flex justify-items-center gap-4">
-          <Link href="/about" className="text-lg font-semibold">
-            About
-          </Link>
-          <Link href="/posts" className="text-lg font-semibold">
-            Posts
-          </Link>
-        </div>
+        <Link href="/posts" className="text-lg font-semibold">
+          Posts
+        </Link>
       </div>
     </header>
   );
