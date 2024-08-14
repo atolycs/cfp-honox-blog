6a8c139112cf578528c9f3d799fa0ac85d07b68e
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-14 17:45:59 +0900

    style: style up

--- app/routes/robots.txt.ts
+++ app/routes/robots.txt.ts
@@ -1,10 +1,7 @@
 import { createRoute } from "honox/factory";
 
 export default createRoute((c) => {
-  const robotsTxt = [
-    "User-agent: *",
-    "Allow: /"
-  ].join("\n")
+  const robotsTxt = ["User-agent: *", "Allow: /"].join("\n");
 
   return c.text(robotsTxt, 200);
 });
