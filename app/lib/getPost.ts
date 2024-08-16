d87ee0aaa53688a8f0ba27f44fca72f9eb57646d
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-08-16 10:31:10 +0900

    [skip ci]: feat: add url check

--- app/lib/getPost.ts
+++ app/lib/getPost.ts
@@ -5,29 +5,38 @@ const allPosts = import.meta.glob<MDX>("../content/blog/*.md", {
   eager: true,
 });
 
-const posts = Object.entries(allPosts).filter(
-    ([_, module]) =>  module.frontmatter?.published || (import.meta.url) && module.frontmatter?.draft 
-  ).map(([path, post]) => {
+const posts = Object.entries(allPosts)
+  .filter(
+    ([_, module]) =>
+      module.frontmatter?.published ||
+      (import.meta.url && module.frontmatter?.draft),
+  )
+  .map(([path, post]) => {
     const entryName = baseName(path);
     const { frontmatter } = post;
     const { default: Component } = post;
     return {
       entryName,
       frontmatter,
-      Component 
-  }}
-  )
+      Component,
+    };
+  });
 
-export const getPosts = () => {
-  if (!import.meta.env.PROD) {  
-    console.log(posts)
+const postMap = (c: Context<Env, any, {}>, allPosts?: any) => {
+  console.log(c.req.url);
+};
+
+export const getPosts = (c: Context<Env, any, {}>) => {
+  postMap(c);
+  if (!import.meta.env.PROD) {
+    console.log(posts);
   }
-  console.log(import.meta.env.BASE_URL)
-  console.log(import.meta.url)
-  console.log(import.meta.env.PROD)
-  console.log(import.meta.env.MODE)
-  return posts
-}
+  console.log(import.meta.env.BASE_URL);
+  console.log(import.meta.url);
+  console.log(import.meta.env.PROD);
+  console.log(import.meta.env.MODE);
+  return posts;
+};
 
 /* export const getPosts = ():PostData[] => {
   console.log(posts)
